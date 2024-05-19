92b3c0258b2704422d9f874f61fd90961beaca1c
donioscode <doniyornormuxammedov@gmail.com>
2024-05-19 12:59:25 +0500

    added appIcon images and add new file NotItems and added LaunchScreen

---
+++ ToDoList/Views/NoItems.swift
@@ -0,0 +1,18 @@
+//
+//  NoItems.swift
+//  ToDoList
+//
+//  Created by doniyor normuxammedov on 19/05/24.
+//
+
+import SwiftUI
+
+struct NoItems: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItems()
+}
